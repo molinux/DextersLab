c751359b931422dc1f9c335898ef478e252c269b
Molinux <cybercrowbr@gmail.com>
2020-07-13 23:32:07 -0300

    One more charts case study

---
+++ charts/projeto-04/js/page.js
@@ -0,0 +1,79 @@
+// Accessing the objects
+var ctx = document.getElementById('monthlySales').getContext('2d');
+var pieCtx = document.getElementById('deptSales').getContext('2d');
+var yearlyLabel = document.getElementById('yearlyTotal');
+var newAmount = document.getElementById('itemAmount');
+var newMonth = document.getElementById('monthId');
+let hikingRadio = document.getElementById('hiking');
+let runningRadio = document.getElementById('running');
+let huntingRadio = document.getElementById('hunting');
+
+// Monthly Totals
+var yearlyTotal = 0;
+
+const monthlySales = new Map();
+
+// Add Sales
+function addSale(){
+  monthlySales.set(newMonth.value, newAmount.value);
+  // console.log(monthlySales);
+  console.log(monthlySales.size);
+}
+
+function findSale(){
+  // console.log(monthlySales.get(1)); // return value
+  // console.log(monthlySales.has(1)); // return true or false
+}
+
+function fillValue() {
+  // monthlySales.delete(1);
+  // console.log(monthlySales);
+}
+
+// Bar chart
+// var monthlySalesChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: [],
+//         datasets: [{
+//             label: '# of Sales',
+//             data: [],
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
+
+// Pie Chart
+// var deptSalesChart = new Chart(pieCtx, {
+//     type: 'pie',
+//     data: {
+//         labels: deptLabels,
+//         datasets: [{
+//             label: '# of Sales',
+//             data: deptSales,
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+        
+//     }
+// })
